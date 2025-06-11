cf6f6bfce64821464afd551d1996123f3ce98269
Nicola Gritti <nicola.gritti.86@gmail.com>
2025-06-11 12:48:52 +0200

    try config

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,17 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
+project = 'PyMIF'
+copyright = 'Nicola Gritt'
+author = 'Nicola Gritt'
+release = 'v0.1.1'
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",        # Google/Numpy style docstrings
-    "sphinx.ext.viewcode",
-    "sphinx_autodoc_typehints",   # Optional: better type hinting
+    # "sphinx.ext.autodoc",
+    # "sphinx.ext.napoleon",        # Google/Numpy style docstrings
+    # "sphinx.ext.viewcode",
+    # "sphinx_autodoc_typehints",   # Optional: better type hinting
     'myst_parser', 
     'autoapi.extension', 
     'sphinx.ext.linkcode'
@@ -26,6 +26,9 @@ def linkcode_resolve(domain, info):
         return None
     filename = info['module'].replace('.', '/')
     return "https://github.com/grinic/pymif/blob/main/%s.py" % filename
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pymif'
+project = 'PyMIF'
 copyright = '2025, Nicola Gritti'
 author = 'Nicola Gritti'
 release = 'v0.1.1'
@@ -14,15 +14,32 @@ release = 'v0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # "sphinx.ext.autodoc",
+    # "sphinx.ext.napoleon",        # Google/Numpy style docstrings
+    # "sphinx.ext.viewcode",
+    # "sphinx_autodoc_typehints",   # Optional: better type hinting
+    'myst_parser', 
+    'autoapi.extension', 
+    'sphinx.ext.linkcode'
+]
+
+autoapi_dirs = ['../../pymif']
+
+def linkcode_resolve(domain, info):
+    
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+    filename = info['module'].replace('.', '/')
+    return "https://github.com/grinic/pymif/blob/main/%s.py" % filename
 
 templates_path = ['_templates']
-exclude_patterns = []
-
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
