c6081ec7caec6910ff8a43f8afcdaf2eced40cd0
Nicola Gritti <nicola.gritti.86@gmail.com>
2025-06-11 14:02:57 +0200

    added sphinx doc

---
+++ doc/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+# this is a trick to make sphinx find the modules in the parent directory
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../../"))
+
+project = 'PyMIF'
+copyright = '2025, Nicola Gritti'
+author = 'Nicola Gritti'
+release = 'v0.1.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser', 'autoapi.extension', 'sphinx.ext.linkcode']
+
+autoapi_dirs = ['../../pymif']
+
+def linkcode_resolve(domain, info):
+    
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+    filename = info['module'].replace('.', '/')
+    return "https://github.com/grinic/documentation-example/blob/main/%s.py" % filename
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,17 @@
+.. PyMIF documentation master file, created by
+   sphinx-quickstart on Wed Jun 11 13:50:26 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+PyMIF documentation
+===================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
