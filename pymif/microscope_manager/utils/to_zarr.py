233a1076781b0859a1fab0cb9458fd1af034fe9e
grinic <nicola.gritti.86@gmail.com>
2025-11-24 15:35:16 +0100

    automatic high val for ome color

--- pymif/microscope_manager/utils/to_zarr.py
+++ pymif/microscope_manager/utils/to_zarr.py
@@ -1,6 +1,7 @@
 import dask.array as da
 from dask.distributed import Client
 from dask.diagnostics import ProgressBar
+import numpy as np
 from numcodecs import Blosc, GZip
 from typing import List, Dict, Any
 from ome_zarr.writer import write_multiscale, add_metadata
@@ -141,7 +142,7 @@ def to_zarr(
         "color": _normalize_color(ch_colors[i]),
         "window": {
             "start": 0,
-            "end": 65000,
+            "end": int(np.clip( 2*np.max(data_levels[-1][-1,i,...].compute()), 100, 65535 )),
             "min": 0,
             "max": 65535
         },
